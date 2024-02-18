5a993a63f94182273aa91e155997c4eae020dfb4
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2024-02-18 20:32:38 +0300

    Add name, password to user class

--- HelloWorls.java
+++ HelloWorls.java
@@ -1,5 +1,7 @@
-public class HelloWorld{
-    public static void main (String[] args{
-
+public class HelloWorld {
+    public static void main(String[] args {
+        User user = new User();
+		user.name = "Alex";
+		user.password = "hghgjgs";
     })
 }
